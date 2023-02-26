6df45239966df73a0c2b5f0f7e93527022350371
Aditi Nanaware <adna4700@colorado.edu>
2023-02-26 16:03:49 -0700

    updated id

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 69d535ea36a74513f4980b577084f83f579ddb33
+AESD_ASSIGNMENTS_VERSION = 67fd63ed1c38974b7c380c35432eae205fa2c3d4
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
