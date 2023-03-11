42e61cd237a51699c503e077a865a8e51c9a57c6
Aditi Nanaware <adna4700@colorado.edu>
2023-03-11 01:08:08 -0700

    assi 7 complete

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-LDD_VERSION = 5a7423a0415ab3d850e94c55c865cf51dc847244
+LDD_VERSION = a510833fa7a3ac60eba7b73358201a377d4ca241
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -17,10 +17,7 @@ LDD_GIT_SUBMODULES = YES
 LDD_MODULE_SUBDIRS = misc-modules/
 LDD_MODULE_SUBDIRS += scull/ 
 
-define LDD_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/scull all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/misc-modules all
-endef
+
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define LDD_INSTALL_TARGET_CMDS
@@ -28,7 +25,7 @@ define LDD_INSTALL_TARGET_CMDS
 	
 	
 	$(INSTALL) -m 0755 $(@D)/misc-modules/* $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)scull/* $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/* $(TARGET_DIR)/usr/bin
 
 	
 endef
